de1c180fa00121b60760442954f477975b81229b
apatel94455 <amipatelr@gmail.com>
2024-08-31 10:46:08 -0500

    upload the first project

---
+++ FirstProject/src/main/java/com/amz/shoppingcart/firstproject/FirstProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.amz.shoppingcart.firstproject;
+
+/**
+ *
+ * @author amipa
+ */
+public class FirstProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ FirstProject/src/main/java/com/amz/shoppingcart/firstproject/Newfirstproject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.amz.shoppingcart.firstproject;
+
+/**
+ *
+ * @author amipa
+ */
+public class Newfirstproject {
+        public int add (int x, int y){
+     return x+y;
+    }
+    
+    public int mul (int x, int y){
+     return x*y;
+    }
+}
